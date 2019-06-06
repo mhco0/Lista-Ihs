83cb125d07cc99d6426050f4fded022ef02b2a41
Thalisson Moura Tavares <tmt2@WINDOWS.CIN.UFPE.BR>
2019-06-06 15:36:43 -0300

    add pcihello ao git

---
+++ pcihello/pcihello_restored/pcihello.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 17.0.0 Build 595 04/25/2017 SJ Standard Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CGX150DF31) Path("C:/Users/tmt2/Desktop/pcihello/pcihello_restored/") File("pcihello.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ pcihello/pcihello_restored/pcihellocore/pcihellocore_bb.v
@@ -0,0 +1,30 @@
+
+module pcihellocore (
+	display_external_connection_export,
+	green_leds_external_connection_export,
+	hexport_external_connection_export,
+	inport_external_connection_export,
+	keys_external_connection_export,
+	pcie_hard_ip_0_pcie_rstn_export,
+	pcie_hard_ip_0_powerdown_pll_powerdown,
+	pcie_hard_ip_0_powerdown_gxb_powerdown,
+	pcie_hard_ip_0_refclk_export,
+	pcie_hard_ip_0_rx_in_rx_datain_0,
+	pcie_hard_ip_0_tx_out_tx_dataout_0,
+	red_leds_external_connection_export,
+	switches_external_connection_export);	
+
+	output	[31:0]	display_external_connection_export;
+	output	[31:0]	green_leds_external_connection_export;
+	output	[31:0]	hexport_external_connection_export;
+	input	[15:0]	inport_external_connection_export;
+	input	[31:0]	keys_external_connection_export;
+	input		pcie_hard_ip_0_pcie_rstn_export;
+	input		pcie_hard_ip_0_powerdown_pll_powerdown;
+	input		pcie_hard_ip_0_powerdown_gxb_powerdown;
+	input		pcie_hard_ip_0_refclk_export;
+	input		pcie_hard_ip_0_rx_in_rx_datain_0;
+	output		pcie_hard_ip_0_tx_out_tx_dataout_0;
+	output	[31:0]	red_leds_external_connection_export;
+	input	[31:0]	switches_external_connection_export;
+endmodule
