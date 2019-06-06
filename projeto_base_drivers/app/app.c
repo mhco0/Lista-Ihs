c966592d1dbd221f159cb81e66a8290127a6e2d9
root <root@de2i-150.(none)>
2019-06-06 20:22:53 -0300

    fix all

--- projeto_base_drivers/app/app.c
+++ projeto_base_drivers/app/app.c
@@ -7,7 +7,7 @@ enum OPS {HEXPORT,INPORT,DISPLAY,RED_LEDS,GREEN_LEDS,KEYS,SWITCHES};
 
 uint32_t number = 0x79794040;
 uint32_t number2 = 2;
-uint32_t number3 = 10;
+uint32_t number3 = 0b10101011;
 unsigned char hexdigit[] = {0x24, 0x79, 0x40, 0x40,
                             0x40, 0x40, 0x40, 0x40, 
                             0xFF, 0x40, 0x40, 0x40,
@@ -38,27 +38,9 @@ int main() {
 
 	printf("%d\n",dev);
 
-	/*for (i=0; i>-1; i++) {
-		//read(dev, &j, 4);
-		k = hexdigit[j & 0xF]
-		  | (hexdigit[(j >>  4) & 0xF] << 8)
-		  | (hexdigit[(j >>  8) & 0xF] << 16)
-		  | (hexdigit[(j >> 12) & 0xF] << 24);
-		k = ~k;
-		write(dev, &k, 4);
-		wait();
-		printf("cu\n");
-	}*/
+	real_write(0,dev,&number3,GREEN_LEDS);
 	
-
-	//real_write(0,dev,hexdigit,RED_LEDS);
-	
-	//real_write(1,dev,hexdigit,GREEN_LEDS);
-	
-	//real_write(2,dev,hexdigit,HEXPORT);
-	
-	
-	read(dev,&cu,SWITCHES);
+	read(dev,&cu,KEYS);
 	
 	printf("%d\n",cu);
 	close(dev);
